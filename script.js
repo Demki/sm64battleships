f9321d38aa3f9c2180c2daabc1b701dbfe98278b
Demki <mordbz@gmail.com>
2023-06-29 18:32:45 +0300

    fix markings save saving "unmarked" squares

--- script.js
+++ script.js
@@ -243,7 +243,7 @@ function genColorsPickerHTML(pickerDiv) {
 }
 
 function saveMarkings() {
-  const data = [...starDivs.entries()].flatMap(([i,d]) => "count" in d.dataset ? [[i,d.dataset.count]] : []);
+  const data = [...starDivs.entries()].flatMap(([i,d]) => "count" in d.dataset && d.dataset.count !== "0" ? [[i,d.dataset.count]] : []);
   const file = new Blob([JSON.stringify(data)], { type: "application/json" });
   const a = document.createElement("a");
 
